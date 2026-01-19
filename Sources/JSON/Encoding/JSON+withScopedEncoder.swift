bd9167c077f06139ded85047a053b7078fee25e0
Tony Freeman <tonyfreeman@users.noreply.github.com>
2026-01-19 21:42:02 +0300

    Fix closure type

--- Sources/JSON/Encoding/JSON+withScopedEncoder.swift
+++ Sources/JSON/Encoding/JSON+withScopedEncoder.swift
@@ -3,12 +3,11 @@ import Stream
 extension JSON {
     public static func withScopedEncoder<T>(
         using writer: StreamWriter,
-        // FIXME: [Concurrency] should be async
-        _ body: (Encoder) throws -> T
+        _ body: (Encoder) async throws -> T
     ) async throws -> T {
         let stream = OutputByteStream()
         let encoder = Encoder(stream)
-        let result = try body(encoder)
+        let result = try await body(encoder)
         try encoder.close()
         try await writer.write(stream.bytes)
         return result
